74554e0bc3032fe91047ec3f1cadab9525615da0
unknown <you@example.com>
2021-02-19 20:49:07 +0530

    Added: displaing units of number using case statement

--- case.sh
+++ case.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-#!/bin/bash
-    echo -n "Enter number : "
-    read n
- 
-len=$(echo $n | wc -c)
-len=$(( $len - 1 ))
- 
-echo "Your number $n in week days: "
-for (( i=1; i<=$len; i++ ))
-do
-    day=$(echo $n | cut -c $i) 
-    case $day in
-0) echo –n “Sunday” ;;
-1) echo –n “Monday” ;;
-2) echo –n “Tuesday” ;;
-3) echo –n “Wednesday” ;;
-4) echo –n “Thursday” ;;
-5) echo –n “Friday” ;;
-6) echo –n “Saturday” ;;
-   esac
-done
+echo "enter a number"
+read num
+num=$num
+if [$num/1000]
+ then
+    echo "Thousands"
+ elif [$num/100]
+  then
+     echo "Hundreds"
+ elif [$num/10]
+   then
+     echo "Tens"
+  else
+     echo "Unit"
+fi
+
 
