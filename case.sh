f4a0e3eef8b3baa7c92641902a0faab10669c79f
unknown <you@example.com>
2021-02-19 20:55:22 +0530

    Added: unit conversion using case statement

--- case.sh
+++ case.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-echo "enter a number"
-read num
-num=$num
-if [$num/1000]
- then
-    echo "Thousands"
- elif [$num/100]
-  then
-     echo "Hundreds"
- elif [$num/10]
-   then
-     echo "Tens"
-  else
-     echo "Unit"
-fi
+echo "1:Feet to inch 2:Inch to feet 3:Feet to Meter 4:Meter to feet"
+echo "enter choice:"
+read choice
+echo "Enter length:"
+read length
+converted_length=0
+case $choice in
+   "1")
+      converted_length=$(($length*12))
+      ;;
+   "2")
+      converted_length=$(($length * 83/1000))
+      ;;
+   "3")
+      converted_length=$(($length * 305/1000))
+      ;;
+   "4")
+      converted_length=$(($length * 3280/1000))
+      ;;
+   *)
+      echo "wrong choice"
+      ;;
+esac
+echo "Converted length :" $converted_length
 
 
