dc2fb8fd59f009a5a0e9d07e94269829e43ae65c
unknown <you@example.com>
2021-02-19 20:44:38 +0530

    Added: code of number in days using case statement

--- case.sh
+++ case.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
+#!/bin/bash
     echo -n "Enter number : "
     read n
  
 len=$(echo $n | wc -c)
 len=$(( $len - 1 ))
  
-echo "Your number $n in words : "
+echo "Your number $n in week days: "
 for (( i=1; i<=$len; i++ ))
 do
-    digit=$(echo $n | cut -c $i) 
-    case $digit in
-        0) echo -n "zero " ;;
-        1) echo -n "one " ;;
-        2) echo -n "two " ;;
-        3) echo -n "three " ;;
-        4) echo -n "four " ;;
-        5) echo -n "five " ;;
-        6) echo -n "six " ;;
-        7) echo -n "seven " ;;
-        8) echo -n "eight " ;;
-        9) echo -n "nine " ;;
-    esac
+    day=$(echo $n | cut -c $i) 
+    case $day in
+0) echo –n “Sunday” ;;
+1) echo –n “Monday” ;;
+2) echo –n “Tuesday” ;;
+3) echo –n “Wednesday” ;;
+4) echo –n “Thursday” ;;
+5) echo –n “Friday” ;;
+6) echo –n “Saturday” ;;
+   esac
 done
+
